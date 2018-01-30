b63fd2cc3ae4fb3a5e841d4b0fe8746f5c9fb688
Daniel Tovesson <daniel.tovesson@me.com>
2018-01-30 09:41:32 +0100

    Redesign EchoTCP1

--- Laboration 3/src/EchoTCP1.java
+++ Laboration 3/src/EchoTCP1.java
@@ -1,34 +1,44 @@
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
 import java.net.Socket;
-import java.net.UnknownHostException;
 
 public class EchoTCP1 {
 
 	public static void main(String[] args) {
 
+		ServerSocket serverSocket;
 		try {
 
-			Socket socket = null;
-			InputStream input = null;
-			OutputStream output = null;
-			try {
-				socket = new Socket("cs.lth.se", 30000);
-				input = socket.getInputStream();
-				output = socket.getOutputStream();
-			} catch (UnknownHostException e) {
-				System.out.println(e);
-				System.exit(1);
-			} catch (IOException e) {
-				System.out.println(e);
-				System.exit(1);
+			// Start connection
+			serverSocket = new ServerSocket(30000);
+			Socket clientSocket = serverSocket.accept();
+			InputStream is = clientSocket.getInputStream();
+			OutputStream os = clientSocket.getOutputStream();
+			BufferedReader in = new BufferedReader(new InputStreamReader(is));
+			PrintWriter out = new PrintWriter(os, true);
+
+			// Variables
+			String inputLine, outputLine;
+
+			// Initiate conversation with client
+			EchoProtocol ep = new EchoProtocol();
+			outputLine = ep.processInput(null);
+			out.println(outputLine);
+
+			// Read lines
+			while ((inputLine = in.readLine()) != null) {
+				outputLine = ep.processInput(inputLine);
+				out.println(outputLine);
+				if (inputLine.equals("q")) {
+					break;
+				}
 			}
 
-			// Protocol echo (e), quit (q)
-			
-			socket.close();
-			
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
