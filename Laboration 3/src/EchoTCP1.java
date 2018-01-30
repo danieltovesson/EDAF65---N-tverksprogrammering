879d1dfa99f3bbf676ef9401ab9fc11c700b879f
Daniel Tovesson <daniel.tovesson@me.com>
2018-01-30 09:04:43 +0100

    Add Socket implementation

--- Laboration 3/src/EchoTCP1.java
+++ Laboration 3/src/EchoTCP1.java
@@ -1,4 +1,36 @@
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.Socket;
+import java.net.UnknownHostException;
 
 public class EchoTCP1 {
 
+	public static void main(String[] args) {
+
+		try {
+
+			Socket socket = null;
+			InputStream input = null;
+			OutputStream output = null;
+			try {
+				socket = new Socket("cs.lth.se", 30000);
+				input = socket.getInputStream();
+				output = socket.getOutputStream();
+			} catch (UnknownHostException e) {
+				System.out.println(e);
+				System.exit(1);
+			} catch (IOException e) {
+				System.out.println(e);
+				System.exit(1);
+			}
+
+			// Protocol echo (e), quit (q)
+			
+			socket.close();
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 }
