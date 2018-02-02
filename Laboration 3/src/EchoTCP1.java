fd37e7f597dd32a9cabbe4eaf18295bf49efe5a6
Daniel Tovesson <daniel.tovesson@me.com>
2018-02-02 12:04:28 +0100

    Finish EchoTCP1

--- Laboration 3/src/EchoTCP1.java
+++ Laboration 3/src/EchoTCP1.java
@@ -4,6 +4,7 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.io.PrintWriter;
+import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -11,37 +12,44 @@ public class EchoTCP1 {
 
 	public static void main(String[] args) {
 
-		while (true) {
+		try {
 
-			ServerSocket serverSocket;
-			try {
+			while (true) {
 
-				// Start connection
-				serverSocket = new ServerSocket(30000);
+				// Start server connection
+				ServerSocket serverSocket = new ServerSocket(24592);
+
+				// Get client socket
 				Socket clientSocket = serverSocket.accept();
+
+				// Get socket streams
 				InputStream is = clientSocket.getInputStream();
 				OutputStream os = clientSocket.getOutputStream();
 				BufferedReader in = new BufferedReader(new InputStreamReader(is));
 				PrintWriter out = new PrintWriter(os, true);
 
-				// Variables
-				String inputLine, outputLine;
+				// Get client address and print it
+				InetAddress inetAddress = clientSocket.getInetAddress();
+				out.println("Server: Connected to " + inetAddress.toString());
 
 				// Read lines
+				String inputLine, outputLine;
 				EchoProtocol ep = new EchoProtocol();
 				while ((inputLine = in.readLine()) != null) {
 					outputLine = ep.processInput(inputLine);
-					out.println(outputLine);
+					out.println("Server: " + outputLine);
+					out.flush();
 					if (inputLine.equals("q")) {
 						clientSocket.close();
+						serverSocket.close();
 						break;
 					}
 				}
 
-			} catch (IOException e) {
-				e.printStackTrace();
 			}
 
+		} catch (IOException e) {
+			e.printStackTrace();
 		}
 	}
 }
